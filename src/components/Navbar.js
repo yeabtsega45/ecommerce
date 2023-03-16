848d9a388356410b0b1abfd86ee2cf0c06f0d7aa
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-16 23:02:34 +0300

    working on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
 
 function Navbar() {
   return (
     <div className="navbar">
       <div className="links">
         <Link to="/">Shop</Link>
-        <Link to="/cart">Cart</Link>
+        <Link to="/cart">
+          <FaShoppingCart />
+        </Link>
       </div>
     </div>
   );
