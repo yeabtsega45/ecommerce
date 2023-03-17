ea6f41c81c62547fa2eea27545fbfeae63712218
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-17 11:06:19 +0300

    working on the data file

--- src/products.js
+++ src/products.js
@@ -1,9 +1,10 @@
-
+import product1 from "./assets/1.png";
 
 export const PRODUCTS = [
-    {id: 1,
+  {
+    id: 1,
     productName: "IPhone",
     price: 999.0,
-    productImage:
-},
-]
+    productImage: product1,
+  },
+];
