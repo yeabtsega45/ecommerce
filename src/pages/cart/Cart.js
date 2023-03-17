b54f6f0f8e54672e7ba455cb9783060a671f2a55
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-17 15:49:26 +0300

    creating cart page

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -2,8 +2,35 @@ import React from "react";
 
 function Cart() {
   return (
-    <div>
-      <div></div>
+    <div className="cart">
+      <div>
+        <h1>Your Cart Items</h1>
+      </div>
+      <div className="cart">
+        {PRODUCTS.map((product) => {
+          if (cartItems[product.id] !== 0) {
+            return <CartItem data={product} />;
+          }
+        })}
+      </div>
+
+      {totalAmount > 0 ? (
+        <div className="checkout">
+          <p> Subtotal: ${totalAmount} </p>
+          <button onClick={() => navigate("/")}> Continue Shopping </button>
+          <button
+            onClick={() => {
+              checkout();
+              navigate("/checkout");
+            }}
+          >
+            {" "}
+            Checkout{" "}
+          </button>
+        </div>
+      ) : (
+        <h1> Your Shopping Cart is Empty</h1>
+      )}
     </div>
   );
 }
