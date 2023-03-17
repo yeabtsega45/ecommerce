d1909d81da95e4e5f1a1fd5c1536f97e7fe85d81
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-17 11:34:27 +0300

    creating a card component with props

--- src/pages/shop/Product.js
+++ src/pages/shop/Product.js
@@ -3,8 +3,14 @@ import React from "react";
 function Product(props) {
   const { id, productName, price, productImage } = props.data;
   return (
-    <div>
-      <div></div>
+    <div className="product">
+      <img src={productImage} alt="images" />
+      <div className="description">
+        <p>
+          <b>{productName}</b>
+        </p>
+        <p>${price}</p>
+      </div>
     </div>
   );
 }
