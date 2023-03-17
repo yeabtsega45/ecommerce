2c474842cbf7bada9fdd315b85b678e684faa33e
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-17 15:27:41 +0300

    creating a file that manages the state of the app using context

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Shop from "./pages/shop/Shop";
 import Cart from "./pages/cart/Cart";
+import { ShopContextProvider } from "./context/shop-context";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Shop />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
+      <ShopContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </ShopContextProvider>
     </>
   );
 }
--- src/pages/shop/Product.js
+++ src/pages/shop/Product.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext, ShopContextProvider } from "../../context/shop-context";
 
 function Product(props) {
   const { id, productName, price, productImage } = props.data;
+  const { addToCart } = useContext(ShopContext);
+
   return (
     <div className="product">
       <img src={productImage} alt="images" />
