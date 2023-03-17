b51f369f3d1c5dab33b63d9f22083b12e0308ab4
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-17 15:41:02 +0300

    creating add to cart function

--- src/pages/shop/Product.js
+++ src/pages/shop/Product.js
@@ -3,7 +3,8 @@ import { ShopContext } from "../../context/shop-context";
 
 function Product(props) {
   const { id, productName, price, productImage } = props.data;
-  const { addToCart } = useContext(ShopContext);
+  const { addToCart, cartItems } = useContext(ShopContext);
+  const cartItemsAmount = cartItems[id];
 
   return (
     <div className="product">
@@ -15,7 +16,7 @@ function Product(props) {
         <p>${price}</p>
       </div>
       <button className="addToCartBttn" onClick={() => addToCart(id)}>
-        Add To Cart
+        Add To Cart {cartItemsAmount > 0 && <>({cartItemsAmount})</>}
       </button>
     </div>
   );
