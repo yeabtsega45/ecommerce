b882302f1e0ca8fc3033eb34a2bfd64a80f255c9
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-17 15:52:53 +0300

    working on cart page

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { ShopContext } from "../../context/shop-context";
 import { PRODUCTS } from "../../products";
 import { CartItem } from "./cart-item";
 import { useNavigate } from "react-router-dom";
+import "./cart.css";
 
 function Cart() {
+  const { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext);
+  const totalAmount = getTotalCartAmount();
+  const navigate = useNavigate();
+
   return (
     <div className="cart">
       <div>
