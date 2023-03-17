f7a5aacb86e8804cda319b80e0d2e2012015e221
yeabtsega45 <yabtsega2022@gmail.com>
2023-03-17 10:32:59 +0300

    linking pages on the routes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Shop from "./pages/shop/Shop";
+import Cart from "./pages/cart/Cart";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" />
-        <Route path="/cart" />
+        <Route path="/" element={<Shop />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </>
   );
