1fa2d473e9864fdf0fb0c3a125c30f7226b9b9ef
yeabtsega45 <yabtsega2022@gmail.com>
2023-05-27 20:38:28 +0300

    creating footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,21 @@ import React from "react";
 
 function Footer() {
   return (
-    <div>
-      <div></div>
+    <div className="footer">
+      <h4>your email adress</h4>
+      <input type="text" />
+      <button>Sign up</button>
+      <div className="icons">
+        <a href="https://www.facebook.com/">
+          <FacebookIcon className="icon" />
+        </a>
+        <a href="https://mail.google.com">
+          <EmailIcon className="icon" />
+        </a>
+        <a href="https://telegram.org/">
+          <TelegramIcon className="icon" />
+        </a>
+      </div>
     </div>
   );
 }
